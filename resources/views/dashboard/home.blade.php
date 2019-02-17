0a839e9171d65ef49b913f2200fd65aa72ef6c7c
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 23:19:17 -0700

    Update dashboard home view

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,5 +1,62 @@
 @extends('dashboard.partial.layout')
 
 @section('page')
-dashboard here
+<div class="row mb-5">
+	<div class="col-12 col-md-4">
+		<div class="card card-body text-center">
+			<p class="display-4 mb-0 font-nunito">{{App\System::count()}}</p>
+			<p class="lead text-muted mb-0 font-nunito">Systems</p>
+		</div>
+	</div>
+	<div class="col-12 col-md-4">
+		<div class="card card-body text-center">
+			<p class="display-4 mb-0 font-nunito">{{App\Service::count()}}</p>
+			<p class="lead text-muted mb-0 font-nunito">Services</p>
+		</div>
+	</div>
+	<div class="col-12 col-md-4">
+		<div class="card card-body text-center">
+			<p class="display-4 mb-0 font-nunito">{{App\Incident::count()}}</p>
+			<p class="lead text-muted mb-0 font-nunito">Incidents</p>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-12 col-md-6">
+		<div class="card">
+			<div class="card-header bg-light lead text-center font-nunito font-weight-bold">Services</div>
+			<div class="list-group list-group-flush" style="max-height: 400px;overflow-y: auto;">
+			@foreach($services as $service)
+				<div class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<div>
+							<span class="text-primary font-nunito font-weight-bold">{{$service->id}}</span>
+							<span class="pl-2 font-nunito font-weight-bold">{{$service->name}}</span>
+						</div>
+						<div>
+							<a class="btn btn-outline-secondary font-nunito btn-sm py-0" href="{{$service->dashboardUrl()}}">View</a>
+						</div>
+					</div>
+				</div>
+			@endforeach
+			</div>
+		</div>
+	</div>
+	<div class="col-12 col-md-6">
+		<div class="card">
+			<div class="card-header bg-light lead text-center font-nunito font-weight-bold">Incidents</div>
+			<div class="list-group list-group-flush" style="max-height: 400px;overflow-y: auto;">
+			@foreach($incidents as $incident)
+				<div class="list-group-item">
+						<div>
+							<p class="font-nunito font-weight-bold mb-0 small">
+								ID: {{$incident->id}}
+							</p>
+						</div>
+				</div>
+			@endforeach
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
