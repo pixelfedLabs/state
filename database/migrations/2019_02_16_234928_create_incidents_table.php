3d44541fc8787e923d6e687ec977535315b5f8d1
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 20:53:22 -0700

    Add Incident model/controller

---
+++ database/migrations/2019_02_16_234928_create_incidents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('service_id')->unsigned()->index();
+            $table->string('title')->nullable();
+            $table->string('state')->nullable();
+            $table->timestamp('resolved_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+}
