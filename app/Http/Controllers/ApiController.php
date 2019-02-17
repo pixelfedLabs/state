c71a35a0258e42f494521fd2c89020c0681af478
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 13:45:51 -0700

    Update ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\{
+	Actor,
 	Incident,
 	IncidentUpdate,
 	Service,
@@ -13,7 +14,8 @@ use League\Fractal;
 use League\Fractal\Serializer\ArraySerializer;
 use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 use App\Transformers\{
-	SystemTransformer
+	ActivityPubActorTransformer,
+	SystemTransformer,
 };
 
 class ApiController extends Controller
@@ -31,5 +33,12 @@ class ApiController extends Controller
 		$systems = System::orderByDesc('created_at')->paginate(10);
 		$res = new Fractal\Resource\Collection($systems, new SystemTransformer());
 		return $this->fractal->createData($res)->toArray();
+	}
+
+	public function actorProfile(Request $request, $id)
+	{
+		$actor = Actor::whereUsername($id)->firstOrFail();
+		$res = new Fractal\Resource\Item($actor, new ActivityPubActorTransformer());
+		return response()->json($this->fractal->createData($res)->toArray(), 200, [], JSON_PRETTY_PRINT);
 	}
 }
