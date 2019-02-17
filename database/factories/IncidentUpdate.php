ebbd7c140b672db677117f85ba64ee5841b9fa99
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 23:18:52 -0700

    Update IncidentUpdate factory

--- database/factories/IncidentUpdate.php
+++ database/factories/IncidentUpdate.php
@@ -6,6 +6,7 @@ use Faker\Generator as Faker;
 $factory->define(App\IncidentUpdate::class, function (Faker $faker) {
 	$incident = App\Incident::inRandomOrder()->firstOrFail();
 	return [
+		'system_id' => $incident->system->id,
 		'service_id' => $incident->service->id,
 		'incident_id' => $incident->id,
 		'slug' => (string) Str::uuid(),
