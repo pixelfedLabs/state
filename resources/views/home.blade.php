cbbfae1a15a39fd1947340fb0885ecd203c2f88e
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 20:56:33 -0700

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="card-body text-center">
+                    <p class="h3 mb-0 font-nunito">Hello, {{Auth::user()->name}}</p>
+                    <p class="lead font-nunito">Welcome to state, to get started you can select an option below.</p>
+                    <p class="mb-0">
+                        <a class="btn btn-outline-primary btn-sm py-1 font-nunito font-weight-bold mx-2" href="#">Dashboard</a>
+                        <a class="btn btn-outline-primary btn-sm py-1 font-nunito font-weight-bold mx-2" href="#">Add Service</a>
+                    </p>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
