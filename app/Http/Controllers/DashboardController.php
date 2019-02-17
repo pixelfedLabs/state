770ef1d0a7629145bc2044f409fa263a19f04b00
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 21:25:22 -0700

    Update DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,13 +6,28 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __construct()
-    {
-    	$this->middleware('auth');
-    }
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-    public function home()
-    {
-    	return view('dashboard.home');
-    }
+	public function home()
+	{
+		return view('dashboard.home');
+	}
+
+	public function systems()
+	{
+		return view('dashboard.systems.home');
+	}
+
+	public function services()
+	{
+		return view('dashboard.services.home');
+	}
+
+	public function incidents()
+	{
+		return view('dashboard.incidents.home');
+	}
 }
