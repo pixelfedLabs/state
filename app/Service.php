7fd43479aef7ffb6bcf1e05de4950fb491270da9
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 23:09:20 -0700

    Update Service model

--- app/Service.php
+++ app/Service.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    public function dashboardUrl()
+    {
+    	return url("/dashboard/services/show/{$this->id}");
+    }
 }
