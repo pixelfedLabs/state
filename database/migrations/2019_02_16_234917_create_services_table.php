e363947390b168241c886d9d4bf1b6533f76442b
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 20:52:32 -0700

    Add Service model/controller

---
+++ database/migrations/2019_02_16_234917_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('system_id')->index();
+            $table->string('name')->nullable();
+            $table->string('slug')->unique()->index();
+            $table->text('description')->nullable();
+            $table->string('tooltip')->nullable();
+            $table->boolean('active')->default(false)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
