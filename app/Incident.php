3bb49dffec5d33a980336313a3777326af459b14
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 23:09:42 -0700

    Update Incident model

--- app/Incident.php
+++ app/Incident.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Incident extends Model
 {
-    public function service()
-    {
-    	return $this->belongsTo(Service::class);
-    }
+	public function service()
+	{
+		return $this->belongsTo(Service::class);
+	}
 }
