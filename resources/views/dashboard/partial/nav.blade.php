57784e8970b0b392757bd44afe93557f275e0ec9
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 23:38:10 -0700

    Update dashboard nav

--- resources/views/dashboard/partial/nav.blade.php
+++ resources/views/dashboard/partial/nav.blade.php
@@ -1,20 +1,23 @@
 <ul class="nav nav-tabs nav-fill">
-  <li class="nav-item">
-    <a class="nav-link font-nunito {{request()->is('dashboard/home')?'active':''}}" href="{{route('dashboard.home')}}">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link font-nunito {{request()->is('dashboard/systems')?'active':''}}" href="{{route('dashboard.systems')}}">Systems</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link font-nunito {{request()->is('dashboard/services')?'active':''}}" href="{{route('dashboard.services')}}">Services</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link font-nunito {{request()->is('dashboard/incidents')?'active':''}}" href="{{route('dashboard.incidents')}}">Incidents</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Agents</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Settings</a>
-  </li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito {{request()->is('dashboard/home')?'active':''}}" href="{{route('dashboard.home')}}">Home</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito {{request()->is('dashboard/systems')?'active':''}}" href="{{route('dashboard.systems')}}">Systems</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito {{request()->is('dashboard/services')?'active':''}}" href="{{route('dashboard.services')}}">Services</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito {{request()->is('dashboard/incidents')?'active':''}}" href="{{route('dashboard.incidents')}}">Incidents</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Agents</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Workers</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Settings</a>
+	</li>
 </ul>
