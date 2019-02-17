9a7bd5292c2275caaa215c656e0e740d186c167b
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 00:27:30 -0700

    Update dashboard nav

--- resources/views/dashboard/partial/nav.blade.php
+++ resources/views/dashboard/partial/nav.blade.php
@@ -3,13 +3,13 @@
 		<a class="nav-link font-nunito {{request()->is('dashboard/home')?'active':''}}" href="{{route('dashboard.home')}}">Home</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link font-nunito {{request()->is('dashboard/systems')?'active':''}}" href="{{route('dashboard.systems')}}">Systems</a>
+		<a class="nav-link font-nunito {{request()->is('dashboard/systems*')?'active':''}}" href="{{route('dashboard.systems')}}">Systems</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link font-nunito {{request()->is('dashboard/services')?'active':''}}" href="{{route('dashboard.services')}}">Services</a>
+		<a class="nav-link font-nunito {{request()->is('dashboard/services*')?'active':''}}" href="{{route('dashboard.services')}}">Services</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link font-nunito {{request()->is('dashboard/incidents')?'active':''}}" href="{{route('dashboard.incidents')}}">Incidents</a>
+		<a class="nav-link font-nunito {{request()->is('dashboard/incidents*')?'active':''}}" href="{{route('dashboard.incidents')}}">Incidents</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link font-nunito disabled" href="#" tabindex="-1" aria-disabled="true">Agents</a>
