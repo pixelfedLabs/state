fa821a9d9e07c15b2110769c6b85e60b386f585c
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 22:04:31 -0700

    Update migration

--- database/migrations/2019_02_16_234909_create_systems_table.php
+++ database/migrations/2019_02_16_234909_create_systems_table.php
@@ -17,7 +17,7 @@ class CreateSystemsTable extends Migration
             $table->increments('id');
             $table->string('name')->nullable();
             $table->string('description')->nullable();
-            $table->string('url')->nullable();
+            $table->string('website')->nullable();
             $table->boolean('active')->default(false)->index();
             $table->timestamps();
         });
