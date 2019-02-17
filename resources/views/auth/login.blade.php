e3ac4d0593ac747c47a9c74bb4e94cf7d3764280
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 20:56:13 -0700

    Update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header bg-white py-1 text-center text-nunito font-weight-bold">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-sm font-nunito font-weight-bold py-1">
                                     {{ __('Login') }}
                                 </button>
 
