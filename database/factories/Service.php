ca2dd61372beae1f04acb1d94c14495df91d6ed6
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 21:44:03 -0700

    Add Service factory

---
+++ database/factories/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(App\Service::class, function (Faker $faker) {
+	$system = App\System::inRandomOrder()->firstOrFail();
+	$name = $faker->unique()->company;
+	$desc = $faker->catchPhrase;
+    return [
+        'system_id' => $system->id,
+        'name' => $name,
+        'slug' => Str::slug($name),
+        'description' => $desc,
+        'tooltip' => $desc,
+        'active' => false
+    ];
+});
