2817784370ebf16b289df2fda7d4bc754bfd044d
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 22:41:53 -0700

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                     <p class="h3 mb-0 font-nunito">Hello, {{Auth::user()->name}}</p>
                     <p class="lead font-nunito">Welcome to state, to get started you can select an option below.</p>
                     <p class="mb-0">
-                        <a class="btn btn-outline-primary btn-sm py-1 font-nunito font-weight-bold mx-2" href="#">Dashboard</a>
+                        <a class="btn btn-outline-primary btn-sm py-1 font-nunito font-weight-bold mx-2" href="{{route('dashboard.home')}}">Dashboard</a>
                         <a class="btn btn-outline-primary btn-sm py-1 font-nunito font-weight-bold mx-2" href="#">Add Service</a>
                     </p>
                 </div>
