3fa67b111dac394d3eca8e3261a4a26c17f9e8d4
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 00:40:21 -0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::group(['prefix' => 'dashboard'], function() {
 	Route::get('systems', 'DashboardController@systems')->name('dashboard.systems');
 	Route::get('systems/show/{id}', 'DashboardController@systemShow');
 	Route::get('services', 'DashboardController@services')->name('dashboard.services');
+	Route::get('services/show/{id}', 'DashboardController@serviceShow');
 	Route::get('incidents', 'DashboardController@incidents')->name('dashboard.incidents');
 
 });
