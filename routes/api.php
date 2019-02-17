90766c1ab53e296068327a070cd4984ddcd0e93c
Daniel Supernault <danielsupernault@gmail.com>
2019-02-16 22:17:59 -0700

    Update routes, add system api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['prefix' => 'v1'], function() {
+
+	Route::get('systems', 'ApiController@systems');
+	
+});
+
