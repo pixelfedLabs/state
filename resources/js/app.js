c1ef4503d7888df67206717f529b2d18500e87ce
Daniel Supernault <danielsupernault@gmail.com>
2019-02-18 13:56:21 -0700

    Remove ExampleComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,8 +20,6 @@ window.Vue = require('vue');
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
