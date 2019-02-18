1fcb19ba0124234ccffcdce767ec35c72b6ed1b9
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 17:39:45 -0700

    Update IncidentController

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Incident;
 
 class IncidentController extends Controller
 {
-    //
+    public function show(Request $request, $id)
+    {
+    	$incident = Incident::with('system')->whereSlug($id)->firstOrFail();
+    	return view('incident.show', compact('incident'));
+    }
 }
