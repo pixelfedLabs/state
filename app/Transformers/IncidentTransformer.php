2b4b9e16560599a2b37584a60dfa2613d367d15f
Daniel Supernault <danielsupernault@gmail.com>
2019-02-18 13:58:31 -0700

    Update IncidentTransformer

--- app/Transformers/IncidentTransformer.php
+++ app/Transformers/IncidentTransformer.php
@@ -7,6 +7,8 @@ use League\Fractal;
 
 class IncidentTransformer extends Fractal\TransformerAbstract {
 
+	protected $defaultIncludes = ['updates'];
+
 	public function transform(Incident $incident)
 	{
 		return [
@@ -15,8 +17,14 @@ class IncidentTransformer extends Fractal\TransformerAbstract {
 			'url' => $incident->url(),
 			'title' => $incident->title,
 			'state' => $incident->state,
+			'day' => $incident->created_at->format('m-d-Y'),
 			'resolved_at' => $incident->resolved_at
 		];
+	}
+
+	public function includeUpdates(Incident $incident)
+	{
+		return $this->collection($incident->updates()->orderByDesc('id')->get(), new IncidentUpdateTransformer);
 	}
 
 }
