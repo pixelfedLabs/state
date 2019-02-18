6f7ff77bc8a570da585d48a42ebf393af079e9f6
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 18:48:02 -0700

    Update Dashboard

--- resources/views/dashboard/incidents/show.blade.php
+++ resources/views/dashboard/incidents/show.blade.php
@@ -14,5 +14,37 @@
 	</div>
 </div>
 <hr>
-
+<div class="row">
+	<div class="col-12 col-md-6">
+		<div class="card">
+			<div class="card-header bg-light lead text-center font-nunito font-weight-bold">Updates</div>
+			<div class="list-group list-group-flush" style="max-height: 400px;overflow-y: auto;">
+			@foreach($updates as $update)
+				<div class="list-group-item">
+					<div class="d-flex justify-content-between">
+						<div>
+							<p class=" font-nunito font-weight-bold mb-0">
+								ID: {{$update->id}}
+							</p>
+							<p class=" font-nunito font-weight-bold mb-0">
+								System: <a class="font-nunito font-weight-bold" href="{{$update->system->dashboardUrl()}}">{{str_limit($update->system->name,18)}}</a>
+							</p>
+							<p class=" font-nunito font-weight-bold mb-0">
+								Service: <a class="font-nunito font-weight-bold" href="{{$update->service->dashboardUrl()}}">{{str_limit($update->service->name,18)}}</a>
+							</p>
+							<p class=" font-nunito font-weight-bold mb-0">
+								State: <span class="font-nunito font-weight-bold">{{$update->state}}</span>
+							</p>
+							<p class="mb-0">{{$update->description}}</p>
+						</div>
+						<div>
+							<a class="btn btn-outline-secondary font-nunito btn-sm py-0" href="{{$update->dashboardUrl()}}">View</a>
+						</div>
+					</div>
+				</div>
+			@endforeach
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
