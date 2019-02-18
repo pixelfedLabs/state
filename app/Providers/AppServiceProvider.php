5b9484e34b82581ec521e5f873dd847143a69d14
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 19:33:30 -0700

    Update ASP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        URL::forceScheme('https');
     }
 
     /**
