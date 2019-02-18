450d001faa3eed69ebcdbd9a6dbb0af8bc88a803
Daniel Supernault <danielsupernault@gmail.com>
2019-02-17 19:33:01 -0700

    Update ActivityPubController

--- app/Http/Controllers/ActivityPubController.php
+++ app/Http/Controllers/ActivityPubController.php
@@ -20,22 +20,82 @@ class ActivityPubController extends Controller
 		$this->fractal = new Fractal\Manager();
 		$this->fractal->setSerializer(new ArraySerializer());
 	}
-	
+
 	public function profile(Request $request, $id)
 	{
 		$actor = Actor::whereUsername($id)->firstOrFail();
-		$res = new Fractal\Resource\Item($actor, new ActivityPubActorTransformer());
-		return response()->json($this->fractal->createData($res)->toArray(), 200, [], JSON_PRETTY_PRINT);
+		$res = new Fractal\Resource\Item($actor, new ActivityPubActorTransformer);
+		return response()->json($this->fractal->createData($res)->toArray(), 200, [
+			'Content-Type' => 'application/activity+json; charset=utf-8'
+		], JSON_PRETTY_PRINT);
 	}
 
-    public function inbox(Request $request)
-    {
+	public function inbox(Request $request)
+	{
 
-    }
+	}
 
-    public function outbox(Request $request, $id)
-    {
-    	$actor = Actor::whereUsername($id)->firstOrFail();
-    	return $actor;
-    }
+	public function outbox(Request $request, $id)
+	{
+		$actor = Actor::whereUsername($id)->firstOrFail();
+		return $actor;
+	}
+
+	public function webfinger(Request $request)
+	{
+		$this->validate($request, ['resource'=>'nullable|string|min:3|max:255']);
+		$resource = $request->input('resource');
+		if(!$resource) {
+			return response('');
+		}
+		$hash = hash('sha256', $resource);
+		$parsed = $this->normalizeProfileUrl($resource);
+		$username = $parsed['username'];
+		$actor = Actor::whereUsername($username)->firstOrFail();
+		$links = [
+			[
+				'rel'  => 'self',
+				'type' => 'application/activity+json',
+				'href' => $actor->permalink(),
+			]
+		];
+		$webfinger = [
+			'subject' => $resource,
+			'aliases' => [$actor->permalink()],
+			'links' => $links
+		];
+		return response()->json($webfinger, 200, [], JSON_PRETTY_PRINT);
+	}
+
+	public function normalizeProfileUrl($url)
+	{
+		if (starts_with($url, 'acct:')) {
+			$url = str_replace('acct:', '', $url);
+		}
+
+		if (!str_contains($url, '@') && filter_var($url, FILTER_VALIDATE_URL)) {
+			$parsed = parse_url($url);
+			$username = str_replace(['/', '\\', '@'], '', $parsed['path']);
+
+			return ['domain' => $parsed['host'], 'username' => $username];
+		}
+		$parts = explode('@', $url);
+		$username = null;
+		$domain = null;
+
+		foreach ($parts as $part) {
+			if (empty($part)) {
+				continue;
+			}
+			if (str_contains($part, '.')) {
+				$domain = filter_var($part, FILTER_VALIDATE_URL) ?
+				parse_url($part, PHP_URL_HOST) :
+				$part;
+			} else {
+				$username = $part;
+			}
+		}
+
+		return ['domain' => $domain, 'username' => $username];
+	}
 }
