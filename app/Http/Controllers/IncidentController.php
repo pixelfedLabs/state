ef8e0f3cfa3e0ab9d40e86b3f3f33d025d462f41
Daniel Supernault <danielsupernault@gmail.com>
2019-02-18 22:46:25 -0700

    Update IncidentController

--- app/Http/Controllers/IncidentController.php
+++ app/Http/Controllers/IncidentController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Incident;
+use League\Fractal;
+use League\Fractal\Serializer\ArraySerializer;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+use App\Transformers\{
+	NoteTransformer
+};
 
 class IncidentController extends Controller
 {
     public function show(Request $request, $id)
     {
     	$incident = Incident::with('system')->whereSlug($id)->firstOrFail();
-    	return view('incident.show', compact('incident'));
+    	if($request->wantsJson()) {
+			$fractal = new Fractal\Manager();
+			$fractal->setSerializer(new ArraySerializer());
+			$res = new Fractal\Resource\Item($incident, new NoteTransformer);
+    		return response()->json($fractal->createData($res)->toArray(), 200, [
+				'Content-Type' => 'application/activity+json; charset=utf-8'
+			]);
+    	} else {
+    		return view('incident.show', compact('incident'));
+    	}
     }
 }
