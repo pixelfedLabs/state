5b805c64999b0484ff603ba7cdc2eae7ad49c977
Daniel Supernault <danielsupernault@gmail.com>
2019-02-18 21:35:52 -0700

    Add CreateNoteTransformer

---
+++ app/Transformers/CreateNoteTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Incident;
+use League\Fractal;
+
+class CreateNoteTransformer extends Fractal\TransformerAbstract {
+
+	public function transform(Incident $incident)
+	{
+		$actor = $incident->system->actor;
+		return [
+			'@context' => [
+				'https://www.w3.org/ns/activitystreams',
+				'https://w3id.org/security/v1',
+			],
+			'id' 					=> $incident->permalink('#activity'),
+			'type' 					=> 'Create',
+			'actor' 				=> $actor->permalink(),
+			'published' 			=> $incident->created_at->toAtomString(),
+			'to' 					=> [
+				"https://www.w3.org/ns/activitystreams#Public"
+			],
+			'cc' 					=> [
+				$actor->permalink('/followers')
+			],
+			'object' => [
+				'id' 				=> $incident->permalink(),
+				'type' 				=> 'Note',
+				'summary'   		=> null,
+				'content'   		=> $incident->title,
+				'inReplyTo' 		=> null,
+				'published'    		=> $incident->created_at->toAtomString(),
+				'url'          		=> $incident->url(),
+				'attributedTo' 		=> $actor->permalink(),
+				'to' 					=> [
+					"https://www.w3.org/ns/activitystreams#Public"
+				],
+				'cc' 					=> [
+					$actor->permalink('/followers')
+				],
+				'sensitive'       	=> false
+			]
+		];
+	}
+
+}
