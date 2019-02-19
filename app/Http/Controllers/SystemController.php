4e3f145d0c1b7111d00189898ff7d95cd0413369
Daniel Supernault <danielsupernault@gmail.com>
2019-02-18 17:46:49 -0700

    Update SystemController

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\System;
 
 class SystemController extends Controller
 {
     public function atomFeed()
     {
-    	$entries = [];
+    	$entries = System::first()->incidents()->orderByDesc('id')->take(10)->get();
     	return response()->view('system.atom', compact('entries'))->header('Content-Type', 'application/atom+xml');
     }
 }
