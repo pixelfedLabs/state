01f85c504123627b550b8192447b96e2fad0f44b
Daniel Supernault <danielsupernault@gmail.com>
2019-02-19 22:03:41 -0700

    Add Agent model/migration

---
+++ app/Agent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Agent extends Model
+{
+    public function url()
+    {
+    	return $this->dashboardUrl();
+    }
+
+	public function dashboardUrl()
+	{
+		return url("/dashboard/agents/show/{$this->id}");
+	}
+
+	public function service()
+	{
+		return $this->belongsTo(Service::class);
+	}
+
+	public function system()
+	{
+		return $this->belongsTo(System::class);
+	}
+}
---
+++ database/migrations/2019_02_19_072201_create_agents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('system_id')->index();
+            $table->bigInteger('service_id')->unsigned()->nullable()->index();
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->uuid('slug')->unique()->index();
+            $table->boolean('local')->default(true);
+            $table->unsignedInteger('frequency')->default(15);
+            $table->string('check_url')->nullable();
+            $table->string('check_text')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agents');
+    }
+}
