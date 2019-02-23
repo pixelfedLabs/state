2596d526567f8574370f20d9c83f32c86c95117e
Daniel Supernault <danielsupernault@gmail.com>
2019-02-23 13:54:30 -0700

    Update AgentCheckController

--- app/Http/Controllers/AgentCheckController.php
+++ app/Http/Controllers/AgentCheckController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\{
+	Agent,
+	AgentCheck
+};
 
 class AgentCheckController extends Controller
 {
-    //
+    public function show(Request $request, $slug, $year, $month, $day)
+    {
+    	$agent = Agent::whereSlug($slug)->firstOrFail();
+
+    	$date = "{$year}-{$month}-{$day}";
+    	$checks = AgentCheck::whereAgentId($agent->id)
+    		->whereDate('created_at', $date)
+    		->count();
+    	abort_if($checks == 0, 404);
+    	return view('uptime.show', compact('agent', 'checks', 'slug', 'year', 'month','day', 'date'));
+    }
 }
