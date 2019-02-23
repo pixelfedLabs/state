d1f4f63eb99bcc826c2229967083eda2fd945365
Daniel Supernault <danielsupernault@gmail.com>
2019-02-23 13:52:20 -0700

    Add UptimePage vue component

---
+++ resources/js/components/UptimePage.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<div class="container">
+	uptime page here
+</div>
+</div>
+</template>
+
+<style type="text/css" scoped>
+	
+</style>
+
+<script type="text/javascript">
+export default {
+	props: ['slug', 'date', 'year', 'month', 'day'],
+	data() {
+		return [
+
+		];
+	}
+}
+</script>
