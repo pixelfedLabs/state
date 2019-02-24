9017ef8f56069b9d1694b4c1c7ffe0d2c1c680b9
Daniel Supernault <danielsupernault@gmail.com>
2019-02-24 00:57:30 -0700

    Add ServicePage component

---
+++ resources/js/components/ServicePage.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="container">
+	service page here.
+</div>
+</template>
+
+<style type="text/css" scoped>
+	
+</style>
+
+<script type="text/javascript">
+export default {
+	props: ['id', 'slug'],
+
+	data() {
+		return [
+
+		];
+	},
+
+	beforeMount() {
+
+	},
+
+	mounted() {
+
+	},
+
+	methods: {
+
+	}
+}
+</script>
