14d3c6f6cd09437805aad72f4eed08549f826b3e
Daniel Supernault <danielsupernault@gmail.com>
2019-02-24 00:58:09 -0700

    Update ServiceController

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -12,4 +12,10 @@ class ServiceController extends Controller
 		$entries = Service::first()->incidents()->orderByDesc('id')->take(10)->get();
 		return response()->view('service.atom', compact('entries'))->header('Content-Type', 'application/atom+xml');
 	}
+
+	public function show(Request $request, $id, $slug)
+	{
+		$service = Service::whereSlug($slug)->whereActive(true)->findOrFail($id);
+		return view('service.show', compact('service'));
+	}
 }
