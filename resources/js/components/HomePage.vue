5e220eb150c0b96287e62bbfb8f03eb43d9d3685
Daniel Supernault <danielsupernault@gmail.com>
2019-02-24 00:57:10 -0700

    Rename vue component

--- resources/js/components/HomePage.vue
+++ resources/js/components/HomePage.vue
@@ -72,9 +72,9 @@
 			</div>
 			<div class="pt-5">
 				<p class="text-center">
-					<a class="font-nunito font-weight-bold text-dark px-2" href="#">About</a>
+					<a class="font-nunito font-weight-bold text-dark px-2" href="/site/about">About</a>
 					<a class="font-nunito font-weight-bold text-dark px-2" href="#">API</a>
-					<a class="font-nunito font-weight-bold text-dark px-2" href="#">Source</a>
+					<a class="font-nunito font-weight-bold text-dark px-2" href="https://github.com/dansup/state">Source</a>
 					<span class="font-nunito font-weight-bold text-lighter px-2">Powered by state</span>
 				</p>
 			</div>
