51fd7c002f355296bd45eaed3d442b9f43e9f1c6
Daniel Supernault <danielsupernault@gmail.com>
2019-02-24 00:56:35 -0700

    Update footer

--- resources/views/dashboard/partial/layout.blade.php
+++ resources/views/dashboard/partial/layout.blade.php
@@ -10,4 +10,13 @@
 </div>
 
 </div>
+<div class="pt-5">
+	<p class="text-center">
+		<a class="font-nunito font-weight-bold text-dark px-2" href="/site/about">About</a>
+		<a class="font-nunito font-weight-bold text-dark px-2" href="#">API</a>
+		<a class="font-nunito font-weight-bold text-dark px-2" href="https://github.com/dansup/state">Source</a>
+		<span class="font-nunito font-weight-bold text-lighter px-2">Powered by state</span>
+	</p>
+</div>
+
 @endsection
--- resources/views/site/subscribe.blade.php
+++ resources/views/site/subscribe.blade.php
@@ -6,4 +6,12 @@
 	<div class="card card-body">Subscribe page here</div>
 
 </div>
+<div class="pt-5">
+	<p class="text-center">
+		<a class="font-nunito font-weight-bold text-dark px-2" href="/site/about">About</a>
+		<a class="font-nunito font-weight-bold text-dark px-2" href="#">API</a>
+		<a class="font-nunito font-weight-bold text-dark px-2" href="https://github.com/dansup/state">Source</a>
+		<span class="font-nunito font-weight-bold text-lighter px-2">Powered by state</span>
+	</p>
+</div>
 @endsection
