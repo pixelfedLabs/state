190f2865e406c34df8cd2c8fb3172cf3833d515a
Daniel Supernault <danielsupernault@gmail.com>
2019-09-19 19:07:39 -0600

    Update AP Job

--- app/Jobs/ActivityPubFanout.php
+++ app/Jobs/ActivityPubFanout.php
@@ -7,10 +7,25 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
 
 class ActivityPubFanout implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $actor;
+    protected $incident;
+    protected $guzzleConcurrency = 10;
+    protected $guzzleTimeout = 5;
+
+    /**
+     * Delete the job if its models no longer exist.
+     *
+     * @var bool
+     */
+    public $deleteWhenMissingModels = true;
 
     /**
      * Create a new job instance.
@@ -19,7 +34,8 @@ class ActivityPubFanout implements ShouldQueue
      */
     public function __construct(Actor $actor, $incident)
     {
-        //
+        $this->actor = $actor;
+        $this->incident = $incident;
     }
 
     /**
@@ -29,6 +45,50 @@ class ActivityPubFanout implements ShouldQueue
      */
     public function handle()
     {
-        //
+        return;
+        $payload = $this->buildActivity();
+        
+        $client = new Client([
+            'timeout'  => $this->guzzleTimeout
+        ]);
+
+        $requests = function($audience) use ($client, $activity, $profile, $payload) {
+            foreach($audience as $url) {
+                $headers = $this->signHeaders($profile, $url, $activity);
+                yield function() use ($client, $url, $headers, $payload) {
+                    return $client->postAsync($url, [
+                        'curl' => [
+                            CURLOPT_HTTPHEADER => $headers, 
+                            CURLOPT_POSTFIELDS => $payload,
+                            CURLOPT_HEADER => true
+                        ]
+                    ]);
+                };
+            }
+        };
+
+        $pool = new Pool($client, $requests($audience), [
+            'concurrency' => $this->guzzleConcurrency,
+            'fulfilled' => function ($response, $index) {
+            },
+            'rejected' => function ($reason, $index) {
+            }
+        ]);
+        
+        $promise = $pool->promise();
+
+        $promise->wait();
+    }
+
+    protected function buildActivity()
+    {
+        $activity = [];
+        return json_encode($activity);
+    }
+
+    protected function signHeaders($profile, $url, $activity)
+    {
+        $headers = [];
+        return $headers;
     }
 }
